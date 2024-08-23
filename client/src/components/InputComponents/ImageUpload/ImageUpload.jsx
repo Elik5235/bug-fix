773e6d38b53415d23c8b0df2e17d05911374334a
Elik <eliseyignatchenko06@gmail.com>
2024-08-23 22:49:28 +0300

    100% image upload

--- client/src/components/InputComponents/ImageUpload/ImageUpload.jsx
+++ client/src/components/InputComponents/ImageUpload/ImageUpload.jsx
@@ -1,41 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classNames from 'classnames';
 import { useField } from 'formik';
 
-const ImageUpload = props => {
-  const [field, meta, helpers] = useField(props.name);
+const ImageUpload = (props) => {
+  const [{ value, ...restField }, meta, helpers] = useField(props.name);
   const { uploadContainer, inputContainer, imgStyle } = props.classes;
-  const onChange = e => {
+
+  useEffect(() => {
+    // Очищаем URL объекта, когда компонент размонтируется или когда значение изменяется
+    return () => {
+      if (value && typeof value !== 'string') {
+        URL.revokeObjectURL(value.preview);
+      }
+    };
+  }, [value]);
+
+  const onChange = (e) => {
+    let file = e.target.files[0];
     const node = window.document.getElementById('imagePreview');
-    const file = e.target.files[0];
-    const imageType = /image.*/;
-    if (!file.type.match(imageType)) {
-      e.target.value = '';
-    } else {
-      field.onChange(file);
-      const reader = new FileReader();
-      reader.onload = () => {
-        node.src = reader.result;
-      };
-      reader.readAsDataURL(file);
+
+    if (!file) {
+      helpers.setValue(null, false);
+      node.src = ''; // Очищаем предварительный просмотр, если файл не выбран
+      return;
     }
+
+    const imageType = /image.*/;
+    if (!file.type?.match(imageType)) {
+      e.target.value = '';
+      helpers.setValue(null, false);
+      node.src = ''; // Очищаем предварительный просмотр, если файл не является изображением
+      return;
+    }
+
+    // Создаем URL объекта для предварительного просмотра
+    file.preview = URL.createObjectURL(file);
+    helpers.setValue(file, false);
+    node.src = file.preview; // Устанавливаем src для предварительного просмотра
   };
+
   return (
     <div className={uploadContainer}>
       <div className={inputContainer}>
         <span>Support only images (*.png, *.gif, *.jpeg)</span>
         <input
-          {...field}
+          {...restField}
           id="fileInput"
           type="file"
           accept=".jpg, .png, .jpeg"
-          onClick={onChange}
+          onChange={onChange}  
         />
-        <label htmlFor="fileInput">Chose file</label>
+        <label htmlFor="fileInput">Choose file</label>
       </div>
       <img
         id="imagePreview"
-        className={classNames({ [imgStyle]: !!field.value })}
+        className={classNames({ [imgStyle]: !!value })}
         alt="user"
       />
     </div>
--- server/src/index.js
+++ server/src/index.js
@@ -3,18 +3,23 @@ const http = require('http');
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('./dbMongo/mongoose');
 const router = require('./router');
 const controller = require('./socketInit');
 const handlerError = require('./handlerError/handler');
+const multerErrorHandler = require('./handlerError/multerHandler');
+const { join } = require('path');
+const { FILES_PATH } = require('./constants');
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/public', express.static('public'));
+app.use('/public', express.static(path.join(FILES_PATH)));
 app.use(router);
+app.use(multerErrorHandler);
 app.use(handlerError);
 
 const server = http.createServer(app);
